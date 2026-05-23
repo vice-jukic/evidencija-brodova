9824bad94e1622eee1e5a29f754a7a0a751ce1d5
Vice Jukic <206936808+vice-jukic@users.noreply.github.com>
2026-05-23 23:23:23 +0200

    Added boat creation route and date formatting

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 from pony import orm
 from datetime import datetime
 
-DB = orm.Database()
+db = orm.Database()
 
 app = Flask(__name__)
 
-class Brod(DB.Entity):
+class Brod(db.Entity):
     id = orm.PrimaryKey(int, auto=True)
     naziv = orm.Required(str)
     tip = orm.Required(str)
@@ -18,12 +18,36 @@ class Brod(DB.Entity):
     zadnji_servis = orm.Optional(datetime)
     opis = orm.Optional(str)
 
-DB.bind(provider="sqlite", filename="database.sqlite", create_db=True)
-DB.generate_mapping(create_tables=True)
+db.bind(provider="sqlite", filename="database.sqlite", create_db=True)
+db.generate_mapping(create_tables=True)
+
+def formatiraj_datum(datum):
+    if datum:
+        return datum.strfitime("%d-%m-%Y")
+    return ""
+
+@app.route("/dodaj-brod", methods=["GET", "POST"])
+@orm.db_session
+def dodaj_brod():
+    if request.method == "POST":
+        Brod(
+            naziv = request.form["naziv"],
+            tip = request.form["tip"],
+            duljina = float(request.form["duljina"]),
+            godina = int(request.form["godina"]),
+            ima_jedra = "ima_jedra" in request.form,
+            ima_tende = "ima_tende" in request.form,
+            oprema = request.form["oprema"],
+            opis = request.form["opis"],
+            zadnji_servis = None
+        )
+
+        return redirect(url_for("home"))
+    return render_template("dodaj_brod.html")
 
 @app.route("/")
 def home():
     return "Evidencija brodova radi!"
 
 if __name__ == "__main__":
-    app.run(port=8080)
+    app.run(port=5000)
